69f50f553b98eba45eaedc9644fe4d58ab2264c0
Max William Medina Castro <williamir1234@gmail.com>
2026-06-17 21:30:54 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Rossy, te tengo un regalo'.split('')
+const text = 'te tengo un regalo'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
